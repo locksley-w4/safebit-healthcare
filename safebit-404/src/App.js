bfcb518a8b6a6f15d3e3cb969ba5c522dc82e1a6
locksley <84179770+locksley-w4@users.noreply.github.com>
2024-11-08 17:40:06 +0500

    router fix

--- safebit-404/src/App.js
+++ safebit-404/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Login from "./pages/login/Login"
-import Signup from "./components/Signup";
+import Signup from "./pages/signup/Signup";
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import Main from "./pages/Main";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -18,15 +18,19 @@ const publicRouter = createBrowserRouter([
     path: "*",
     element: <Login />,
   },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ]);
 
 localStorage.setItem("isAuth", "true");
-localStorage.clear("isAuth");
+// localStorage.clear("isAuth");
 
 function App() {
   let [isAuth, setIsAuth] = useState(false);
   useEffect(() => {
-    setIsAuth(localStorage.getItem("isAuth"));
+    setIsAuth(localStorage.getItem("isAuth") === "true");
     console.log(isAuth);
   }, []);
   // return <div className="App">{isAuth ? <Login /> : <Signup />}</div>;
