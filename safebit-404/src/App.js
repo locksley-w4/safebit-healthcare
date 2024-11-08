1a26aa5ae2f9864010b5af89f5ed5156f1c57b2d
Amirshoh Akhmedov <104301048+Emris06@users.noreply.github.com>
2024-11-08 16:53:04 +0500

    push

--- safebit-404/src/App.js
+++ safebit-404/src/App.js
@@ -1,11 +1,15 @@
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <div className="App">
-      <Nav></Nav>
-    </div>
-  );
+  let [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    setIsAuth(localStorage.getItem("userId"));
+    console.log(isAuth);
+  }, []);
+  return <div className="App">{isAuth ? <Login /> : <Signup />}</div>;
 }
 
 export default App;
