190be469814fcf68f28c3baa85a4060b5b7eb4e0
locksley <84179770+locksley-w4@users.noreply.github.com>
2024-11-08 17:23:39 +0500

    router init

--- safebit-404/src/App.js
+++ safebit-404/src/App.js
@@ -2,14 +2,39 @@ import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Main from "./pages/Main";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const privateRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <NotFoundPage />,
+  },
+]);
+const publicRouter = createBrowserRouter([
+  {
+    path: "*",
+    element: <Login />,
+  },
+]);
+
+localStorage.setItem("isAuth", "true");
+localStorage.clear("isAuth");
 
 function App() {
   let [isAuth, setIsAuth] = useState(false);
   useEffect(() => {
-    setIsAuth(localStorage.getItem("userId"));
+    setIsAuth(localStorage.getItem("isAuth"));
     console.log(isAuth);
   }, []);
-  return <div className="App">{isAuth ? <Login /> : <Signup />}</div>;
+  // return <div className="App">{isAuth ? <Login /> : <Signup />}</div>;
+  return (
+    <>
+      <RouterProvider router={isAuth ? privateRouter : publicRouter} />
+    </>
+  );
 }
 
 export default App;
---
+++ safebit-404/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+        Main page
+    </main>
+  )
+}
+
+export default Main
